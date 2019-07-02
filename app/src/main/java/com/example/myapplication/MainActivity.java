7c5cb57fc64c17e4b7c1b8b990ae40661ea7eafa
xch752 <1628265481@qq.com>
2019-07-02 14:52:34 +0800

    add SplashActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mCalcButton;
+    private TextView mConvertedTextView;
+    private EditText mAmountEditText;
+    private Spinner mForSpinner,mHomSpinner;
+    private String[] mCurrencies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCalcButton = (Button)findViewById(R.id.btn_calc);
+        mForSpinner = (Spinner)findViewById(R.id.spn_for);
+        mHomSpinner = (Spinner)findViewById(R.id.spn_hom);
+        mAmountEditText =(EditText)findViewById(R.id.edt_amount);
+        mConvertedTextView = (TextView)findViewById(R.id.txt_converted);
+
+        ArrayList<String> arrayList = ((ArrayList<String>)
+                getIntent().getSerializableExtra(SplashActivity.KEY_ARRAYLIST));
+        Collections.sort(arrayList);
+        mCurrencies = arrayList.toArray(new String[arrayList.size()]);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
     }
 }
